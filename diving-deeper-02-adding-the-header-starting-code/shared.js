9bac1868e2548ab3c3b2f107a6bde0212bec8ff7
Mitaly Sen <senmitaly13@gmail.com>
2021-08-12 21:34:51 +0530

    added mobile navigation javascript add class and remove class

--- diving-deeper-02-adding-the-header-starting-code/shared.js
+++ diving-deeper-02-adding-the-header-starting-code/shared.js
@@ -1,19 +1,34 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 document.querySelectorAll('.plan .button').forEach(element => {
     element.addEventListener('click', () => {
-        backdrop.style.display = 'block'
-        modal.style.display = 'block'
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     })
 })
 
 
 document.querySelector('.modal__action--negative').addEventListener('click', closeModalAndBackdrop)
 
-backdrop.addEventListener('click', closeModalAndBackdrop)
+backdrop.addEventListener('click', () => {
+    mobileNav.classList.remove('open');
+    // mobileNav.style.display = 'none';
+    closeModalAndBackdrop();
+});
+
+toggleButton.addEventListener('click', () => {
+    backdrop.classList.add('open');
+    mobileNav.classList.add('open');
+    // backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+});
 
 function closeModalAndBackdrop() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
 }
